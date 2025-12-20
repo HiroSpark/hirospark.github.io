243459b7e0620cd9a5dfdcfd4bc0e5b84190de89
HiroSpark <contact.hirospark@gmail.com>
2025-12-20 19:41:06 +0900

    トップページのみ制作

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,79 @@
 import Image from "next/image";
 
-export default function Home() {
+interface Article {
+  id: number;
+  title: string;
+  slug: string;
+  published_at: string;
+  emoji: string;
+  path: string;
+  user: {
+    username: string;
+    name: string;
+  };
+}
+
+export default async function Home() {
+  // 一度の取得件数の制限にかからない程の記事数しかないのでループ処理はしてない
+  const res = await fetch(
+    "https://zenn.dev/api/articles?username=hirospark&order=latest"
+  );
+  const data = await res.json();
+  const articles: Article[] = data.articles;
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex min-h-screen items-center justify-center font-sans bg-zinc-100">
+      <main className="max-w-xl min-h-screen w-full mx-6">
+        <h1 className="font-bold text-2xl mt-12">HiroSpark</h1>
+
+        <div className="my-12">
+          <p className="mb-4">Webや組版について興味があります。</p>
+          <div className="font-semibold flex gap-6">
+            <p>
+              <a
+                href="https://zenn.dev/hirospark"
+                className="flex gap-1 flex-row content-center transition delay-10 duration-200 ease-[ease] hover:transform-[translateX(4px)]"
+              >
+                <Image src="/arrow-right.svg" alt="" height={18} width={18} />
+                Zenn
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://github.com/HiroSpark"
+                className="flex gap-1 flex-row content-center transition delay-100 duration-200 ease-[ease] hover:transform-[translateX(4px)]"
+              >
+                <Image src="/arrow-right.svg" alt="" height={18} width={18} />
+                GitHub
+              </a>
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+
+        {/* featured articles */}
+        <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-7">
+          {articles.map((article) => {
+            const url = "https://zenn.dev" + article.path;
+            return (
+              <li key={article.id}>
+                <a
+                  href={url}
+                  className="flex flex-row gap-1 justify-between items-start h-full p-4 rounded-xl border-1 border-zinc-400 bg-white font-semibold hover:bg-zinc-50 hover:border-zinc-600"
+                >
+                  <span>{article.title}</span>
+                  <Image
+                    src="/arrow-right-circle.svg"
+                    alt=""
+                    className=""
+                    width={24}
+                    height={24}
+                  />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        {/* the others */}
       </main>
     </div>
   );
